2c921301d26fa78589af5d4bbb749d4ff0fa35a7
Denys Santos <denysnando@gmail.com>
2015-09-08 21:01:06 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'devise'
+gem 'simple_form'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+# Use unicorn as the app server
+ gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ginga::Application.config.secret_key_base = 'fbaa0293c9a6bf1a63dd73e6392cc66e2a96b0fccfe20be70bba7a9c522bc17d06f521ccf702e864cb66ef85913d4e3f8c00676b093101aa0ad9e3ec8342af7d'
