aef7608dd088c6dd91351874dd6454ed98e55185
diogohonorio <diogo.h.guilherme@gmail.com>
2015-09-14 21:22:43 -0300

    assets para admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Ginga::Application.routes.draw do
+  root 'welcomes#index'
   devise_for :users
+  resources :welcomes, only: [:index]
   namespace :admin do
     resources :products
     resources :categories
+    resources :welcomes, only: [:index]
   end
 end
